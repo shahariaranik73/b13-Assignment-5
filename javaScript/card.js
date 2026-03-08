5251752c4994c3167273e3dbd28b090cf3c0d187
shahariaranik73 <shahariaranik73@gmail.com>
2026-03-08 14:29:17 +0600

    finally finished Modal section

--- javaScript/card.js
+++ javaScript/card.js
@@ -71,26 +71,26 @@ const displayIssues = (issues) => {
 
         let borderColor = "";
         let bg_Color = "";
-
+            // open Close bg-color effecte
         if (issue.status === "open") {
             borderColor = "border-green-600";
         }
         else if (issue.status === "closed") {
             borderColor = "border-purple-600";
         }
-
+            // priority
         if (issue.priority === "high") {
-            bg_Color = "!bg-red-300";
+            bg_Color = "bg-red-300";
         }
         else if (issue.priority === "medium") {
-            bg_Color = "!bg-amber-100";
+            bg_Color = "bg-amber-100";
         }
         else {
-            bg_Color = "!bg-purple-200";
+            bg_Color = "bg-purple-200";
         }
 
         div.innerHTML = ` 
-            <div id="${issue.id}" onclick="loadCardDetail(${issue.id})" class="space-y-4 p-3 shadow-md rounded-2xl h-full border-t-4 ${borderColor}">
+            <div id="${issue.id}" onclick="loadCardDetail(${issue.id})" class="space-y-4 p-3 shadow-lg rounded-2xl h-full border-t-4 ${borderColor}">
                 
                 <!-- Priority badge -->
                 <div class="flex justify-end p-2">
@@ -135,8 +135,8 @@ const displayIssues = (issues) => {
 }
 
 const loadCardDetail = async (id) => {
-    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
-    
+    const url =`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+
     const res = await fetch(url);
     const details = await res.json();
 
@@ -144,13 +144,65 @@ const loadCardDetail = async (id) => {
     // console.log(details)
 }
 
-const displayCardDetails = (word) =>{
+// modal Render card
 
-
-    console.log(word)
+const displayCardDetails = (word) => {
+    // console.log(word)
     const cardBox = document.getElementById("card-modalContainer");
+
+    // status bg-color
+
+     if (word.status === "open") {
+            bgColor ="bg-green-600 text-white";
+        }
+        else if (word.status === "closed") {
+            bgColor ="bg-purple-600 text-white";
+        }
+
+    // color effect
+
+     if (word.priority === "high") {
+            bg_Color = "bg-red-600 text-white";
+        }
+        else if (word.priority === "medium") {
+            bg_Color = "bg-amber-600 text-white";
+        }
+        else {
+            bg_Color = "bg-purple-600 text-white";
+        }
+
+
+    cardBox.innerHTML = `
+                <div class="space-y-4">
+                        <h2 class="text-2xl font-bold">${word.title}</h2>
+                        <div class="flex items-center gap-4">
+                            <button class="rounded-full border-2 px-3 py-1 ${bgColor}">${word.status}</button>
+                            <p class="opacity-50">${word.updatedAt}</p>
+                        </div>
+
+                            <div class="flex gap-2 mb-2.5 flex-wrap">
+                                ${word.labels.map(label => `
+                                <span class="bg-amber-200 text-black rounded px-2 py-1 text-sm">
+                                ${label}</span>`).join('')}
+                            </div>
+                        
+                            <p class="opacity-60">${word.description}</p>
+
+                        <div class="flex justify-between items-center p-2 bg-[#F8FAFC] ">
+                            <div>
+                             <h2 class="">Assignee:</h2>
+                                <h2 class="font-bold">${word.author}</h2>
+                            </div>
+
+                            <div class="pr-4 text-center">
+                                <h2 class="">Priority:</h2>
+                                <button class="rounded-full border-2 px-3 py-1 ${bg_Color}">${word.priority}</button>
+                            </div>
+
+                        </div>
+                   </div>
     
-    // cardBox.innerHTML= " Hi Me is Anik from JS"
+    `;
 
     document.getElementById("my_modal_5").showModal()
 }
